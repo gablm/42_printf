969a46134b047802134a574159890b0ff60c50cc
Gabriel Lima <44784408+gablm@users.noreply.github.com>
2023-10-08 14:41:04 +0100

    Update test.c

--- testing/test.c
+++ testing/test.c
@@ -1,13 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gfragoso <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/08 14:38:55 by gfragoso          #+#    #+#             */
+/*   Updated: 2023/10/08 14:38:55 by gfragoso         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../ft_printf.h"
 #include <stdio.h>
 
-int main(int argc, char **argv) 
+int	main(int argc, char **argv)
 {
+	int	s;
+	int	h;
+
 	(void)argc;
 	(void)argv;
-	int s = ft_printf("% 1s", "");
+	s = ft_printf("% 1s", "");
 	printf("\n======\nn1=%i\n======\n", s);
-	int h = printf("% 1s", "");
+	h = printf("% 1s", "");
 	printf("\n======\nn2=%i\n======\n", h);
 	return (0);
 }
